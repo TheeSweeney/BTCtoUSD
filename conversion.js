c06c21c460011b4334c7da2c961ef828434e8e5c
TheeSweeney <bsweeney930@gmail.com>
2017-09-05 18:02:15 -0400

    MVP complete;

--- conversion.js
+++ conversion.js
@@ -2,15 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 	function BTCtoUSD(){
-		chrome.tabs.executeScript({
-			code: "console.log(window.getSelection() + 'stuff')"
+		chrome.tabs.executeScript(null, {
+			code: "var a = (new Number(window.getSelection())) + 0; a;"
+		}, function(selection){
+			if(typeof selection[0] === 'number'){
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", "https://blockchain.info/ticker", false)
+				xhr.send();
+
+				alert(JSON.parse(xhr.response).USD['15m'] * selection)
+			}else{
+				alert("please select a number and only a number - no letters or symbols.")
+			}
 		})
 		
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://blockchain.info/ticker", false)
-		xhr.send();
-
-		console.log(JSON.parse(xhr.response).USD['15m'])
 	}
 
 	var title = "Convert BTC to USD";
